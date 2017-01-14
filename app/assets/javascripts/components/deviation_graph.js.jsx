f6971decca950def636814d00a93ea433678d323
Grandman <root@grandman73.ru>
2017-01-14 05:01:45 +0400

    front fixes

--- app/assets/javascripts/components/deviation_graph.js.jsx
+++ app/assets/javascripts/components/deviation_graph.js.jsx
@@ -6,6 +6,7 @@ class DeviationGraph extends React.Component {
       pointsCount: props.pointsCount
     };
     this.getGraph = this.getGraph.bind(this);
+    this.handleChangeManualEquation = this.handleChangeManualEquation.bind(this);
   };
 
   componentWillReceiveProps(nextProps) {
@@ -21,28 +22,38 @@ class DeviationGraph extends React.Component {
   render() {
     return (
       <div>
-        <button onClick={this.getGraph}>Построить график с аномалией</button>
+        <button onClick={this.getGraph}>Построить график с аномалией</button><br/>
+        Функция аномалии: <input type='text' onChange={this.handleChangeManualEquation} />
         {this.state.deviation_equation &&
             <div>Функция: {this.state.deviation_equation}</div>}
         <Chart elementId='deviation_chart_div' rows={[this.state.coords]} />
-        {this.state.anomalies} <br/>
-        {this.state.all}
+        {this.state.sliding_window &&
+            <div>
+              Метод скользящего окна:<br/>
+              {this.state.sliding_window.anomalies} <br/>
+              {this.state.sliding_window.all}
+            </div>
+        }
       </div>
     );
   };
   getGraph(state, e) {
     self = this;
-    $.post('/api/deviation_graph', {equation: self.state.equation, points_count: self.state.pointsCount}, function(data) {
+    $.post('/api/deviation_graph', {equation: self.state.equation, points_count: self.state.pointsCount, deviation_equation: self.state.manual_equation}, function(data) {
       self.setState(data);
       self.calcSlidingWindow(data.row.join());
     }, "json");
     return 'ok';
-  }
+  };
   calcSlidingWindow(row) {
     self = this;
     $.post('/api/anomaly_detector', {type: 'sliding_window', row: row}, function(data) {
-      self.setState(data);
+      self.setState({sliding_window: data});
     }, "json");
     return 'ok';
+  };
+
+  handleChangeManualEquation(e) {
+    this.setState({manual_equation: e.target.value});
   }
 }
