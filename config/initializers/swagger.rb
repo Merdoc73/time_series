c773f2c884c79d5deb2e7fce35acb2ab7faa0789
Grandman <root@grandman73.ru>
2017-02-01 00:54:04 +0400

    fix url for swagger
    fix url for swagger

--- config/initializers/swagger.rb
+++ config/initializers/swagger.rb
@@ -3,8 +3,8 @@ Swagger::Docs::Config.register_apis({
     # the extension used for the API
     :api_extension_type => nil,
     # the output location where your .json files are written to
-    :api_file_path => "public/",
-    :base_path => "http://127.0.0.1:3000/",
+    :api_file_path => Rails.root.join("public/"),
+    :base_path => "https://time-series-1.herokuapp.com/",
     # if you want to delete all .json files at each generation
     :clean_directory => true,
     # add custom attributes to api-docs
